b06e8f454ff49c505176d6787ed590b4c1298c0a
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-05-22 15:47:31 +0500

    added meal-item, meal-grid and added content to mealpage, also added sqlite-3 database

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,7 +1,22 @@
-export default function Meals() {
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
+export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meals <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your recipe and start cooking</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share a recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
