28a159a62b42623a4cbf38fedb168a931089343c
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-05-22 16:23:46 +0500

    loading logic is moved to the mealsPage

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -2,9 +2,14 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+
+function Meals() {
+  const meals = getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
 export default function MealsPage() {
-  const meals = getMeals();
   return (
     <>
       <header className={classes.header}>
@@ -17,7 +22,11 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Loading meals.....</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
